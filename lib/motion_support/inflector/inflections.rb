ae9090803de07dac09e8f0a1f9402f8b49fa6594
hookercookerman <hookercookerman@gmail.com>
2012-07-23 11:11:31 +0100

    making sure we get the append_prepend_array for inflector

--- lib/motion_support/inflector/inflections.rb
+++ lib/motion_support/inflector/inflections.rb
@@ -1,4 +1,4 @@
 require 'bubble-wrap/loader'
-BubbleWrap.require('motion/inflector/*.rb') do
+BubbleWrap.require('motion/**/*.rb') do
   file('motion/inflector/inflections.rb').depends_on('motion/core_ext/array/prepend_append.rb')
 end
